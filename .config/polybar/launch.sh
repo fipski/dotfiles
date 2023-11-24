f9f9b0b700f083e0a1f41c37ee352a0509497ebc
Philipp <philipp.vt@gmail.com>
2023-11-24 10:26:51 +0100

    polybar multi monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,19 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m
+    if [ $m == 'DP-2' ]; then
+      polybar --reload bar1 2>&1 | tee -a /tmp/polybar1.log & disown 
+    elif [ $m == 'DP-4' ]; then
+      polybar --reload bar2 2>&1 | tee -a /tmp/polybar1.log & disown 
+    fi
+  done
+else
+  polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+  polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
 echo "Bars launched..."
